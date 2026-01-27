e2d4864338358be3bb9166fcd24ea9aac874c318
frekeasuquo <asuquondifreke06@gmail.com>
2026-01-27 23:56:25 +0000

    v1.2

--- services/payment-service/index.js
+++ services/payment-service/index.js
@@ -4,13 +4,22 @@ import cors from 'cors'
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost/3000"
+    origin: "http://localhost:3000"
 }));
 
 app.use(express.json());
 
 app.post("/payment-service", async (req,res) => {
-    
+    const {cart} = req.body
+    // Assume that we get the cookie and decrypt the user ID
+
+    const userId = '123'
+
+    // Todo: Payment
+    console.log("Api endpoint hit")
+    // Kafka
+
+    return res.status(200).send("Payment Successful")
 });
 
 app.use((err, req, res, next) => {
